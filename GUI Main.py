df45adff163fd2461244cdc71a447bc7e7d2b9ca
danerdstig <164146168+danerdstig@users.noreply.github.com>
2024-08-02 08:23:39 +1200

    Update GUI Main.py
    I made my buttons equal sized, and added a banner to the bottom

--- GUI Main.py
+++ GUI Main.py
@@ -23,11 +23,11 @@ class OrderingGUI:
         frame = ttk.Frame(self.notebook)
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
-        frame.columnconfigure(0, minsize=60, weight=0)
-        frame.columnconfigure(1, minsize=275, weight=0)
-        frame.columnconfigure(2, minsize=275, weight=0)
-        frame.columnconfigure(3, minsize=275, weight=0)
-        frame.columnconfigure(4, minsize=60, weight=0)
+        frame.columnconfigure(0, minsize=60, weight=1)
+        frame.columnconfigure(1, minsize=275, weight=1)
+        frame.columnconfigure(2, minsize=275, weight=1)
+        frame.columnconfigure(3, minsize=275, weight=1)
+        frame.columnconfigure(4, minsize=60, weight=1)
         frame1 = content_frames.this_is_a_test()
         frame2 = content_frames.orderingtab_1()
         frame3 = content_frames.orderingtab_2()
@@ -42,24 +42,42 @@ class ContentFrames:
         self.notebook = notebook
 
     def this_is_a_test(self):
-        frame = ttk.Frame(self.notebook)
+        frame = ttk.Frame(self.notebook, borderwidth=0, relief='flat')
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
+        frame.columnconfigure(0, minsize=70)
+        frame.columnconfigure(1, minsize=265)
+        frame.columnconfigure(2, minsize=275)
+        frame.columnconfigure(3, minsize=265)
+        frame.columnconfigure(4, minsize=70)
+        frame.grid_propagate(False)
 
         label1 = tk.Label(frame, background="#000000", foreground="#ffffff", padx=30, pady=30,
                           text="THIS IS A TEST", font=("Times", 24, "bold"))
-        label1.grid(row=0, column=0, sticky="NSEW")
+        label1.grid(row=0, column=0, sticky="NSEW", rowspan=5)
+
+# --------------------- menu buttons copy paste
+        back_button = tk.Button(frame, text="←", font="80", width=7, height=3)
+        back_button.grid(row=3, column=0, sticky="SW")
+        next_button = tk.Button(frame, text="→", font="80", width=7, height=3, command=orderingtab_1)
+        next_button.grid(row=3, column=4, sticky="SW")
+        current_tab = tk.Label(frame, text='Kahawa Coffee™ - Ordering Tab 1', font="3000", bg='light gray')
+        current_tab.grid(row=3, column=2, sticky="SNEW", pady=10)
+        banner = tk.Label(frame, bg='light gray', width=200, height=4)
+        banner.grid(row=3, column=0, columnspan=5, sticky="SWEN")
+        banner.lower()
+
         return frame
 
     def orderingtab_1(self):
-        frame = ttk.Frame(self.notebook)
+        frame = ttk.Frame(self.notebook, borderwidth=0, relief='flat')
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
-        frame.columnconfigure(0, minsize=60)
-        frame.columnconfigure(1, minsize=275)
+        frame.columnconfigure(0, minsize=70)
+        frame.columnconfigure(1, minsize=265)
         frame.columnconfigure(2, minsize=275)
-        frame.columnconfigure(3, minsize=275)
-        frame.columnconfigure(4, minsize=60)
+        frame.columnconfigure(3, minsize=265)
+        frame.columnconfigure(4, minsize=70)
         frame.grid_propagate(False)
 
         label = ttk.Label(frame, text='Tab 2')
@@ -71,50 +89,48 @@ class ContentFrames:
 #        text = tk.Text(frame, height=10, width=40)
 #        text.grid(row=3, column=3, sticky="NSEW", pady=5)
 
-        back_button = tk.Button(frame, text="←", font="40", width=10, height=2)
-        back_button.grid(row=3, column=0, sticky="W")
-        next_button = tk.Button(frame, text="→", font="40", width=20, height=2)
-        next_button.grid(row=3, column=4, sticky="E")
-
-        CurrentTab = ttk.Label(frame, text='Kahawa Coffee™ - Ordering Tab 1', font="3000")
-        CurrentTab.grid(row=3, column=2, sticky="EW", pady=10)
+# --------------------- menu buttons copy paste
+        back_button = tk.Button(frame, text="←", font="80", width=7, height=3)
+        back_button.grid(row=3, column=0, sticky="SW")
+        next_button = tk.Button(frame, text="→", font="80", width=7, height=3)
+        next_button.grid(row=3, column=4, sticky="SW")
+        current_tab = tk.Label(frame, text='Kahawa Coffee™ - Ordering Tab 1', font="3000", bg='light gray')
+        current_tab.grid(row=3, column=2, sticky="SNEW", pady=10)
+        banner = tk.Label(frame, bg='light gray', width=200, height=4)
+        banner.grid(row=3, column=0, columnspan=5, sticky="SWEN")
+        banner.lower()
 
         return frame
 
     def orderingtab_2(self):
-        frame = ttk.Frame(self.notebook)
+        frame = ttk.Frame(self.notebook, borderwidth=0, relief='flat')
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
 
-        frame.columnconfigure(0, minsize=60)
-        frame.columnconfigure(1, minsize=275)
+        frame.columnconfigure(0, minsize=70)
+        frame.columnconfigure(1, minsize=265)
         frame.columnconfigure(2, minsize=275)
-        frame.columnconfigure(3, minsize=275)
-        frame.columnconfigure(4, minsize=60)
+        frame.columnconfigure(3, minsize=265)
+        frame.columnconfigure(4, minsize=70)
         frame.grid_propagate(False)
 
-        label = ttk.Label(frame, text="Tab 3")
-        label.grid(row=0, column=0, sticky="NSEW", pady=10)
-
-        entry = ttk.Entry(frame)
-        entry.grid(row=1, column=0, sticky="NSEW", pady=5)
-
-        button = ttk.Button(frame, text="Submit")
-        button.grid(row=2, column=0, sticky="NSEW", pady=5)
-
-        text = tk.Text(frame, height=10, width=40)
-        text.grid(row=3, column=0, sticky="NSEW", pady=5)
-
-        checkbox_var = tk.BooleanVar()
-        checkbox = ttk.Checkbutton(frame, text="Check me", variable=checkbox_var)
-        checkbox.grid(row=4, column=0, sticky="NSEW", pady=5)
+# --------------------- menu buttons copy paste
+        back_button = tk.Button(frame, text="←", font="80", width=7, height=3)
+        back_button.grid(row=3, column=0, sticky="SW")
+        next_button = tk.Button(frame, text="→", font="80", width=7, height=3)
+        next_button.grid(row=3, column=4, sticky="SW")
+        current_tab = tk.Label(frame, text='Kahawa Coffee™ - Ordering Tab 2', font="3000", bg='light gray')
+        current_tab.grid(row=3, column=2, sticky="SNEW", pady=10)
+        banner = tk.Label(frame, bg='light gray', width=200, height=4)
+        banner.grid(row=3, column=0, columnspan=5, sticky="SWEN")
+        banner.lower()
 
         return frame
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("945x660")
+    root.geometry("950x660")
 #    root.overrideredirect(True)
     root.configure(bg='white')
     root.resizable(False, False)
